c4056bce247ba4d2bc8332d067294acdaf055c81
Jerry Fallon <jerryfallon@Jerrys-MacBook-Pro.local>
2016-12-22 13:20:03 -0700

    Long overdue clean up!
    - Remove dependency on jQuery
    - Clean up js, html, css
    - Add manifest.yml for CF deployment

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-$(document).ready(function() {
-	var message = $('#message');
-	message.focus();
+(() => {
+  const title = 'nopechat';
 
-	setInterval(function() {
-		if(message.val()) {
-			message.val('');
-			$('.success').fadeIn(300).fadeOut(300);
-		}
-	}, 1000);
+  setInterval(() => {
+    const message = document.querySelector('#message');
 
-	var title = 'nopechat';
-	var count = 0;
-	setInterval(function() {
-		$('.title').text(title.substring(0,count++-3));
-		if(count > 13) count = 0;
-	}, 200);
-});
+    if (message.value) {
+      message.value = '';
+      const element = document.querySelector('#success');
+      element.className = 'fadeout';
+      setTimeout(() => {
+        element.className = '';
+      }, 500);
+    }
+  }, 1000);
+
+  let count = 0;
+  setInterval(() => {
+    document.querySelector('#title').innerText = title.substring(0, count++ - 3);
+    if (count > title.length + 10) count = 0;
+  }, 200);
+})();
